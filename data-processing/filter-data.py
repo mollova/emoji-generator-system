e48a69244f66b45a2eb3e783516741169aaccb26
Margarita Mollova <mmollova@vmware.com>
2024-02-04 12:57:31 +0200

    Choose emojis

--- data-processing/filter-data.py
+++ data-processing/filter-data.py
@@ -2,30 +2,46 @@ import emoji
 import csv
 import re
 
-file = open("datasets/raw-data/face_with_steam_from_nose.csv")
-type(file)
+from typing import List
 
-csvreader2 = csv.reader(file)
+dataset_emoji_mapping = {
+    "datasets/raw-data/face_holding_back_tears.csv": '🥹',
+    "datasets/raw-data/face_with_steam_from_nose.csv": '😤',
+    "datasets/raw-data/face_with_tears_of_joy.csv": '😂',
+    "datasets/raw-data/loudly_crying_face.csv": '😭',
+    "datasets/raw-data/smiling_face_with_heart-eyes.csv": '😍'
+}
 
-header = []
-header = next(csvreader2)
+def sanitize_file_by_emoji(file_content: List[str], target_emoji: str) -> List[str]:
+    filtered = []
 
-rows = []
-for row in csvreader2:
-    rows.append(row)
+    for row in file_content:
+        row_str = str(row)
+        dist = emoji.distinct_emoji_list(row_str)
+        if len(dist) == 1 and target_emoji in dist:       
+            result = re.sub(target_emoji,'',row_str) + target_emoji
+            filtered.append(result)
+
+    return filtered
+
+for filename in dataset_emoji_mapping:
+    #read file
+    file = open(filename)
+    csvreader2 = csv.reader(file)
+
+    header = []
+    header = next(csvreader2)
+
+    rows = []
+    for row in csvreader2:
+        rows.append(row)
 
 
-# Define the target emoji
-target_emoji = '😤'
+    # get the target emoji
+    target_emoji = dataset_emoji_mapping.get(filename)
 
-# Filter texts with only the target emoji
+    # filter rows with only the target emoji
+    filtered = sanitize_file_by_emoji(rows, target_emoji)
 
-filtered = []
-for row in rows:
-   row_str = str(row)
-   dist = emoji.distinct_emoji_list(row_str)
-   if len(dist) == 1 and target_emoji in dist:       
-       result = re.sub(target_emoji,'',row_str) + target_emoji
-       filtered.append(result)
-
-print(len(filtered))
+    print(filtered[0][-1])
+    print(len(filtered))
