41847f722b5b8dfab1063977dbd0bb27f3aacf25
Bozhidar Dimitrov <dbozhidar@vmware.com>
2024-02-04 13:20:35 +0200

    Clean rows from @tags http links and quotes

--- data-processing/filter-data.py
+++ data-processing/filter-data.py
@@ -12,36 +12,62 @@ dataset_emoji_mapping = {
     "datasets/raw-data/smiling_face_with_heart-eyes.csv": '😍'
 }
 
+def clean_row(text: str) -> str:
+    # remove emojis, remove links, anonymize user mentions
+    text = re.sub(r'[\[\]]', '', text)
+    text = re.sub(r'\"', '', text)
+    text = re.sub('\s+', ' ', text).strip()
+    clean = ""
+    for word in text.split(" "):
+        if (word.startswith('@') or word.startswith('\'@')) and len(word) >= 1:
+            pass
+        elif word.startswith('http') or 'http' in word:
+            pass
+        elif word.startswith('https') or 'https' in word:
+            pass
+        else:
+            clean += word + " "
+
+    return clean.strip()
+
 def sanitize_file_by_emoji(file_content: List[str], target_emoji: str) -> List[str]:
     filtered = []
 
     for row in file_content:
         row_str = str(row)
         dist = emoji.distinct_emoji_list(row_str)
-        if len(dist) == 1 and target_emoji in dist:       
-            result = re.sub(target_emoji,'',row_str) + target_emoji
-            filtered.append(result)
+        if len(dist) == 1 and target_emoji in dist:
+            result = re.sub(target_emoji,'',row_str)
+            filtered.append(clean_row(result) + ' ' + target_emoji)
 
     return filtered
 
-for filename in dataset_emoji_mapping:
-    #read file
-    file = open(filename)
-    csvreader2 = csv.reader(file)
 
-    header = []
-    header = next(csvreader2)
+def collect_all_data(dataset_emoji_mapping: dict) -> List[str]:
+    all_data = []
+    for filename in dataset_emoji_mapping:
+        #read file
+        file = open(filename)
+        csvreader2 = csv.reader(file)
 
-    rows = []
-    for row in csvreader2:
-        rows.append(row)
+        header = []
+        header = next(csvreader2)
+
+        rows = []
+        for row in csvreader2:
+            rows.append(row)
+
+        # get the target emoji
+        target_emoji = dataset_emoji_mapping.get(filename)
+
+        # filter rows with only the target emoji
+        filtered = sanitize_file_by_emoji(rows, target_emoji)
+
+        all_data.extend(filtered)
+
+    return all_data
 
 
-    # get the target emoji
-    target_emoji = dataset_emoji_mapping.get(filename)
-
-    # filter rows with only the target emoji
-    filtered = sanitize_file_by_emoji(rows, target_emoji)
-
-    print(filtered[0][-1])
-    print(len(filtered))
+result = collect_all_data(dataset_emoji_mapping=dataset_emoji_mapping)
+for r in result:
+    print(r)
