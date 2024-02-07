6601871205abbe7a6ec60a7845b7e1253ac55a2b
Margarita Mollova <mmollova@vmware.com>
2024-02-07 15:06:57 +0200

    Fixed conflicts in processing data

--- data-processing/processing_data.py
+++ data-processing/processing_data.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd
-import csv
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-import preprocess_data
-import pickle
-from sklearn.pipeline import Pipeline
-
-train_dataset_name = 'datasets/data/train_data_five_emojis.csv'
-test_dataset_name = 'datasets/data/test_data_five_emojis.csv'
-=======
 import preprocess_data
 import csv
 import pandas as pd
@@ -19,18 +6,10 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
 train_dataset_name = 'datasets/data/train_data_five_emojis.csv'
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
 
 train_data_dictionary_filepath = 'datasets/in-progress-data/dictionary.dict'
 test_data_dictionary_filepath = 'datasets/in-progress-data/test-dictionary.dict'
 
-<<<<<<< HEAD
-bow_nb_model = 'classifier_models/bow_nb.pkl'
-tfidf_nb_model = 'classifier_models/tfidf_nb.pkl'
-
-
-=======
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
 def emoji_to_integer(df: pd.DataFrame):
     df.loc[df.emojis == '😭', 'emojis' ] = 0
     df.loc[df.emojis == '😂', 'emojis' ] = 1
@@ -54,12 +33,7 @@ def create_dataframe(dataset_filename: str, dictionary_path: str) -> pd.DataFram
     # transform the emojis from string to integer
     emoji_to_integer(df)
     return df
-<<<<<<< HEAD
 
-
-=======
-    
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
 def bag_of_words(df: pd.DataFrame, vectorizer) -> pd.DataFrame:
     doc_term_matrix = vectorizer.fit_transform(df.tweets)
     vocab = vectorizer.get_feature_names_out()
@@ -75,16 +49,6 @@ def train_model_NB(vectorizer) -> MultinomialNB:
     clf.fit(doc_term_df, target_values)
     return clf
 
-<<<<<<< HEAD
-
-def save_trained_model(model: MultinomialNB, model_filepath: str):
-    with open(model_filepath, 'wb') as file:
-        pickle.dump(model, file)
-
-
-def load_trained_model(model_filepath: str) -> MultinomialNB:
-    with open(model_filepath, 'rb') as file:
-=======
 def save_trained_model(model: MultinomialNB, model_filepath: str, vectorizer, vectorizer_filepath: str):
     with open(model_filepath, 'wb') as file:  
         pickle.dump(model, file)
@@ -94,7 +58,6 @@ def save_trained_model(model: MultinomialNB, model_filepath: str, vectorizer, ve
 
 def load_trained_model(model_filepath: str, vectorizer_filepath: str):
     with open(model_filepath, 'rb') as file:  
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
         model = pickle.load(file)
 
     with open(vectorizer_filepath, 'rb') as file:  
@@ -110,43 +73,18 @@ def test_bag_of_words(df: pd.DataFrame, vectorizer) -> pd.DataFrame:
 
 
 def calculate_accuracy(clf: MultinomialNB, vectorizer, dataset_filename: str):
-<<<<<<< HEAD
-    df = create_dataframe(dataset_filename, test_data_dictionary_filepath)
-    doc_term_df = test_bag_of_words(df, vectorizer=vectorizer)
-    target_values = df.emojis.astype(int)
-=======
     df = create_dataframe(dataset_filename, test_data_dictionary_filepath) 
     doc_term_df = test_bag_of_words(df, vectorizer=vectorizer) 
     target_values = df.emojis.astype(int)   
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
 
     predicted_emojis = clf.predict(doc_term_df)
     print(predicted_emojis[:10])
     print(target_values[:10])
     print("Accuracy: ", accuracy_score(target_values, predicted_emojis))
-<<<<<<< HEAD
 
 
-def bow_and_nb():
-    vectorizer = CountVectorizer()
-    clf = train_model_NB(vectorizer)
-    save_trained_model(clf, bow_nb_model)
 
-    model = load_trained_model(bow_nb_model)
-    calculate_accuracy(model, vectorizer, test_dataset_name)
-
-
-def tfidf_and_nb():
-    tfidf_vectorizer = TfidfVectorizer()
-    clf = train_model_NB(tfidf_vectorizer)
-    save_trained_model(clf, tfidf_nb_model)
-
-    model = load_trained_model(tfidf_nb_model)
-    calculate_accuracy(model, tfidf_vectorizer, test_dataset_name)
 
 # bow_and_nb()
 
 tfidf_and_nb()
-
-=======
->>>>>>> c6f15b2 (Added 5 more emojis and moved classifiers to a separate file)
